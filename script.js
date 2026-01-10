4bd683235f08fb4badc5321f4112c77858c16006
7aa-7 <loganheine@outlook.com>
2026-01-10 13:36:29 -0500

    Update

--- script.js
+++ script.js
@@ -7,4 +7,9 @@ playerForm.addEventListener("submit", function (event) {
   const playerFormData = new FormData(playerForm);
   
   const player = playerFormData.get("player_name")
+
+  if (player == "logan")
+  {
+	  window.location.href = "/Rehoboth/Logan.html"
+  }
 })
