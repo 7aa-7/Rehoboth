7d6d66e27e3b487383c7db6cdb71ab1eafdffe17
7aa-7 <loganheine@outlook.com>
2026-01-10 13:21:31 -0500

    Added page for logan and changed files

---
+++ script.js
@@ -0,0 +1,10 @@
+const playerForm = document.getElementById("playerNameForm");
+
+playerForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  
+  
+  const playerFormData = new FormData(playerForm);
+  
+  const player = playerFormData.get("player_name")
+})
