1cb48544d39016a5095afd7a2a6ed8966152c3a0
7aa-7 <loganheine@outlook.com>
2026-01-15 19:49:40 -0500

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,10 @@ playerForm.addEventListener("submit", function (event) {
 	  window.location.href = "Logan.html"
   }
 })
+
+//Supabase
+
+const subabase = createClient(
+	'https://fautgukxaxrqsvyibasy.supabase.co'
+	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZhdXRndWt4YXhycXN2eWliYXN5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg0MzIwNzIsImV4cCI6MjA4NDAwODA3Mn0.59ZslvggYCgsBHoCB05KQNZSCB8DkjCgGpH4DY1LU2A'
+)
